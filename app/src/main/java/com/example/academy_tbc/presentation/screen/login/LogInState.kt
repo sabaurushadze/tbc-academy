7fd2a45658a06a6f4722c9ba83f666fa994dba37
Saba <geotdsu@gmail.com>
2025-11-21 11:32:54 +0400

    Quiz 5 done

--- app/src/main/java/com/example/academy_tbc/presentation/screen/login/LogInState.kt
+++
@@ -1,7 +0,0 @@
-package com.example.academy_tbc.presentation.screen.login
-
-data class LogInState(
-    val email: String = "",
-    val password: String = "",
-    val isLoading: Boolean = false,
-)
